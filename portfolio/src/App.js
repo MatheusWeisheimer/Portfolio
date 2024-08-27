f0b136b87458566cbdeafc8049d8e6cbdcc7df57
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-27 16:04:23 -0300

    create darkMode state

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import './App.css';
-import Home from './components/Home/Home'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import TechStack from './components/TechStack/TechStack';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true)
+  const backgroundColor = darkMode ? "#191919" : "#F5F5F5"
+
+  function switchDarkMode() {
+    setDarkMode(prev => !prev)
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div className="App" style={{backgroundColor: backgroundColor}}>
+      <Header 
+        darkMode={darkMode} switchDarkMode={switchDarkMode} 
+      />
     </div>
   );
 }
