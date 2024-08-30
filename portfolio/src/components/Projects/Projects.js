874b7a11118bc1cd7fa451230cd73780491ecdaf
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-30 13:42:34 -0300

    create Project section component

---
+++ portfolio/src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import ProjectCard from "../ProjectCard/ProjectCard"
+
+function Projects({darkMode}) {
+    return(
+        <section name="projects">
+            <ProjectCard darkMode={darkMode}/>
+        </section>
+    )
+}
+
+export default Projects
