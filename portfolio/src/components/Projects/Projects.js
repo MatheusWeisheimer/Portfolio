61f38bc7ab95235940c82187ede96aace9d9d8c7
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-31 16:40:44 -0300

    Update Projects component

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,9 +1,23 @@
+import './Projects.css'
+
 import ProjectCard from "../ProjectCard/ProjectCard"
 
 function Projects({darkMode}) {
     return(
         <section name="projects">
-            <ProjectCard darkMode={darkMode}/>
+            <div className="heading_content_container">
+                <h2 className={`heading ${darkMode ? "darkmode_heading" : "lightmode_heading"}`}>
+                    Projects
+                </h2>
+                <p className={`content ${darkMode ? "darkmode_content" : "lightmode_content"}`}>
+                    My Personal Projects
+                </p>
+            </div>
+            <div className="projects">
+                <ProjectCard darkMode={darkMode}/>
+                <ProjectCard darkMode={darkMode}/>
+                <ProjectCard darkMode={darkMode}/>
+            </div>
         </section>
     )
 }
