534faba78a127e65ae9a7606e08e3f23c967b4cb
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-31 16:41:12 -0300

    Update Header component

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import { Link, Element, animateScroll as scroll } from 'react-scroll'
+import ColoredIcon from '../icons/ColoredIcon'
 import UncoloredIcon from '../icons/UncoloredIcon'
 import './Header.css'
 
 function Header({darkMode, switchDarkMode}) {
     return (
         <header className='header'>
+            <div className="darkMode_switch" onClick={() => switchDarkMode()}>   
+                <ColoredIcon name={darkMode ? "ColoredSun" : "ColoredMoon"} width="2.5rem"/>
+                <span className={darkMode ? "darkmode_heading" : "lightmode_heading"}>
+                    {darkMode ? "Light Mode" : "Dark Mode"}
+                </span>
+            </div>
             <nav>
                 <ul className={`nav_options ${darkMode ? "darkmode_content" : "lightmode_content"}`}>
                     <li>
                         <Link to="home" smooth={true} duration={500}>
                             Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="about" smooth={true} duration={500}>
-                            About
                         </Link>
                     </li>
                     <li>
@@ -30,8 +32,12 @@ function Header({darkMode, switchDarkMode}) {
                 </ul>
             </nav>
             <div className="contact">
-                <a href="https://github.com/MatheusWeisheimer" target="blank"><UncoloredIcon name="UncoloredGithub" darkMode={darkMode} width="1.87rem"/></a>
-                <a href="https://www.linkedin.com/in/matheus-weisheimer-30325a242/" target="blank"><UncoloredIcon name="UncoloredLinkedin" darkMode={darkMode} width="1.87rem"/></a>
+                <a href="https://github.com/MatheusWeisheimer" target="blank">
+                    <UncoloredIcon name="UncoloredGithub" width="1.87rem" darkMode={darkMode}/>
+                </a>
+                <a href="https://www.linkedin.com/in/matheus-weisheimer-30325a242/" target="blank">
+                    <UncoloredIcon name="UncoloredLinkedin" width="1.87rem" darkMode={darkMode}/>
+                </a>
             </div>
         </header>
     )
