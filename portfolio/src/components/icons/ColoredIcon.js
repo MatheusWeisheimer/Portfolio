f6f11656a27865a6f67a42582d49d8572b97c635
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-31 16:35:51 -0300

    add bug prevention

--- portfolio/src/components/icons/ColoredIcon.js
+++ portfolio/src/components/icons/ColoredIcon.js
@@ -3,6 +3,10 @@ function ColoredIcon({name, width}) {
     const req = require(`./iconComponents/${name}.js`);
     const IconComponent = req.default
 
+    if (!req.default) {
+        return null
+    }
+
     return (
         <IconComponent width={width}/>
     )
