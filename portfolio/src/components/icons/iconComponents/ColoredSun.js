64afedfb4e89e9042a53c4a6110862cd9cb0e7b1
Matheus Weisheimer <matheusweish@gmail.com>
2024-08-31 16:39:31 -0300

    Create new svg icon components

---
+++ portfolio/src/components/icons/iconComponents/ColoredSun.js
@@ -0,0 +1,9 @@
+function ColoredSun({width}) {
+    return (
+        <svg style={{width: width, height: width}} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="20" cy="20" r="20" fill="#FFD542"/>
+        </svg>
+    )  
+}
+
+export default ColoredSun
