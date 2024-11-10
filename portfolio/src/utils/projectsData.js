cc7d07ad561ac83ac80514be5f080005d6cbcd05
Matheus Weisheimer <matheusweish@gmail.com>
2024-11-10 11:53:18 -0300

    Create projects data

---
+++ portfolio/src/utils/projectsData.js
@@ -0,0 +1,20 @@
+const projectsData = [
+    {
+        image: "Portfolio",
+        title: "Portfolio",
+        desc: "",
+        tecs: ["Html", "Css", "Js", "Reactjs"],
+        livePreview: "",
+        viewCode: "https://github.com/MatheusWeisheimer/Portfolio",
+    },
+    {
+        image: "PlayStation",
+        title: "PlayStation Website",
+        desc: "",
+        tecs: ["Html", "Css", "Js", "Reactjs"],
+        livePreview: "https://playstation-website-clone.vercel.app/",
+        viewCode: "https://github.com/MatheusWeisheimer/PlayStation-Website-Clone",
+    },
+]
+
+export default projectsData
