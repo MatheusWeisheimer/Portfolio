254dac1d03b79aa3dfa1f02c441eea5264c2e14e
Matheus Weisheimer <matheusweish@gmail.com>
2024-11-11 10:31:46 -0300

    Include new project

--- portfolio/src/utils/projectsData.js
+++ portfolio/src/utils/projectsData.js
@@ -1,19 +1,27 @@
 const projectsData = [
     {
-        image: "Portfolio",
+        image: "Portfolio.jpg",
         title: "Portfolio",
-        desc: "",
+        desc: "My personal portfolio website, designed to showcase my skills, projects, and experience as a developer.",
         tecs: ["Html", "Css", "Js", "Reactjs"],
         livePreview: "",
         viewCode: "https://github.com/MatheusWeisheimer/Portfolio",
     },
     {
-        image: "PlayStation",
+        image: "PlayStation.jpg",
         title: "PlayStation Website",
-        desc: "",
+        desc: "This project aims to be a fully responsive frontend clone of the official PlayStation website, built with React.",
         tecs: ["Html", "Css", "Js", "Reactjs"],
         livePreview: "https://playstation-website-clone.vercel.app/",
         viewCode: "https://github.com/MatheusWeisheimer/PlayStation-Website-Clone",
+    },
+    {
+        image: "tbd.png",
+        title: "Java + SQL Project",
+        desc: "For my next project, I’m excited to apply my recently acquired skills in Java and SQL by developing a data-centric application that combines these technologies.",
+        tecs: ["Java", "Sqlite"],
+        livePreview: "",
+        viewCode: "",
     },
 ]
 
